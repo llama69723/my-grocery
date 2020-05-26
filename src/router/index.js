06de9414a234b86def560bd3a10c6620a60fa97e
llama69723 <brownie69723@gmail.com>
2020-05-26 15:33:42 +0900

    my-grocery

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Index from '../views/Index.vue'
+import Login from '../views/Login.vue'
+import SignUp from '../views/SignUp.vue'
 
 Vue.use(VueRouter)
 
@@ -11,13 +14,21 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path:'/index',
+    name:'index',
+    conponent: Index
+  },
+  {
+    path:'/login',
+    name:'login',
+    conponent: Login
+  },
+  {
+    path:'/signUp',
+    name:'signUp',
+    conponent: SignUp
+  },
+
 ]
 
 const router = new VueRouter({
